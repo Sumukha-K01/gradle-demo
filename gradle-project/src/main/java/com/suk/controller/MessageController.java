697cf21f5d2e2270b9b9fea78c284a39d3bac138
Sumukha <suk@extremenetworks.com>
2024-08-27 15:17:35 +0530

    commit-1

---
+++ gradle-project/src/main/java/com/suk/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.suk.controller;
+
+import com.suk.service.MessageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+    @Autowired
+    private MessageServiceImpl service;
+
+    @GetMapping("/")
+    public String getMessage() {
+        return service.getMessage();
+    }
+}
---
+++ gradle-project/src/test/java/com/suk/GradleDemoApplicationTest.java
@@ -0,0 +1,12 @@
+package com.suk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GradleDemoApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
