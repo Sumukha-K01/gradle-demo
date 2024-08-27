79a1e61cfee473f9b47759b83829eae90ea8f95b
Sumukha <suk@extremenetworks.com>
2024-08-27 16:25:22 +0530

    workflow changes-8

--- gradle-project/src/test/java/com/suk/GradleDemoApplicationTest.java
+++ gradle-project/src/test/java/com/suk/GradleDemoApplicationTest.java
@@ -1,12 +1,22 @@
 package com.suk;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.suk.controller.MessageController;
 @SpringBootTest
 public class GradleDemoApplicationTest {
-
+    @Autowired
+    private MessageController controller;
     @Test
     void contextLoads() {
+    }
+    @Test
+    void testGetMessage() {
+        String message = controller.getMessage();
+        System.out.println("Message: " + message);
+        Assertions.assertNotNull(message);
     }
 }
